99991024f9211f19e84fa9c139f79d6082815826
Miguel Ordonez Camara <miguelordonezcamara@hotmail.com>
2026-04-20 17:55:14 -0600

    update

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 const THEMES = {
   ligh: 'caramellatte',
@@ -11,18 +11,25 @@ const THEMES = {
 export class ThemeService {
 
   private currentTheme: string = THEMES.darK
+  isDark = signal(true);
+
 
   constructor() {
     this.currentTheme = localStorage.getItem('theme') || THEMES.darK;
+    this.tooglePropertie(this.currentTheme);
     this.applyTheme(this.currentTheme)
   }
 
-  toggleTheme(): void {
+  tooglePropertie(theme: string): void {
+    const res = (theme === THEMES.darK) ? true : false;
+    this.isDark.set(res);
+  }
 
+  toggleTheme(): void {
     this.currentTheme = this.currentTheme === THEMES.darK ? THEMES.ligh : THEMES.darK;
+    this.tooglePropertie(this.currentTheme);
     this.applyTheme(this.currentTheme);
     localStorage.setItem('theme', this.currentTheme);
-
   }
 
   private applyTheme(theme: string): void {
